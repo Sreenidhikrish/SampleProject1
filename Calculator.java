62cf25a1671feb4cd898dc9e2e5404d41d4cc8db
Sreenidhi <srinidhi.krishna@gmail.com>
2016-10-25 17:19:32 -0700

    Initial Commit: Sample

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+/**
+ * Created by admin on 10/24/2016.
+ */
+public class Calculator {
+
+    SampleClass  o = new SampleClass();
+
+
+    public String fun(String p)
+    {
+ if(p!=null)
+       return p;
+else
+    return new String("sorry");
+    }
+class SampleClass {
+
+
+
+}
+public static void main( String [] e)
+{
+
+}
+
+}
